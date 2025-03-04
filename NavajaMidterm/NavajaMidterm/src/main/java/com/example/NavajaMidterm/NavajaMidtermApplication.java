133f0d786d59570a26b06e0d4884eb0cdf112fa9
Kurt <151386675+kurt-navaja@users.noreply.github.com>
2025-03-04 16:12:22 +0800

    Initial Commit

---
+++ NavajaMidterm/NavajaMidterm/src/main/java/com/example/NavajaMidterm/NavajaMidtermApplication.java
@@ -0,0 +1,13 @@
+package com.example.NavajaMidterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavajaMidtermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavajaMidtermApplication.class, args);
+	}
+
+}

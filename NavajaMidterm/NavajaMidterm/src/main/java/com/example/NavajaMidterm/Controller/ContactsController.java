adb738e3660d0baa3abf0e8aafce9067a52816fb
Kurt <151386675+kurt-navaja@users.noreply.github.com>
2025-03-04 17:07:10 +0800

    Updated

--- NavajaMidterm/NavajaMidterm/src/main/java/com/example/NavajaMidterm/Controller/ContactsController.java
+++ NavajaMidterm/NavajaMidterm/src/main/java/com/example/NavajaMidterm/Controller/ContactsController.java
@@ -1,4 +1,4 @@
-package Controller;
+package com.example.NavajaMidterm.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- NavajaMidterm/NavajaMidterm/src/main/java/com/example/NavajaMidterm/NavajaMidtermApplication.java
+++ NavajaMidterm/NavajaMidterm/src/main/java/com/example/NavajaMidterm/NavajaMidtermApplication.java
@@ -2,10 +2,8 @@ package com.example.NavajaMidterm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"Controller"})
 public class NavajaMidtermApplication {
 
     public static void main(String[] args) {

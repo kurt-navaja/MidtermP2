06c98fb9d893525aa1ec4bdee7c8861848a7bb5a
Kurt <151386675+kurt-navaja@users.noreply.github.com>
2025-03-04 16:40:37 +0800

    Updated

--- NavajaMidterm/NavajaMidterm/src/main/java/com/example/NavajaMidterm/NavajaMidtermApplication.java
+++ NavajaMidterm/NavajaMidterm/src/main/java/com/example/NavajaMidterm/NavajaMidtermApplication.java
@@ -2,12 +2,13 @@ package com.example.NavajaMidterm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"Controller"})
 public class NavajaMidtermApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NavajaMidtermApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(NavajaMidtermApplication.class, args);
+    }
 }
